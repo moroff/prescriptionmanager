173fc8788a3e7758f5a6e70de65c6320ddfdc798
Dieter Moroff <dieter@myhousehold.eu>
2018-02-28 23:08:36 +0100

    GUI translation German and Enlish supported

--- src/main/java/info/moroff/prescriptionmanager/PrescriptionManagerApplication.java
+++ src/main/java/info/moroff/prescriptionmanager/PrescriptionManagerApplication.java
@@ -1,8 +1,12 @@
 package info.moroff.prescriptionmanager;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 @SpringBootApplication
@@ -16,4 +20,11 @@ public class PrescriptionManagerApplication {
     public Java8TimeDialect java8TimeDialect() {
         return new Java8TimeDialect();
     }
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+	    SessionLocaleResolver slr = new SessionLocaleResolver();
+	    slr.setDefaultLocale(Locale.GERMAN);
+	    return slr;
+	}
 }
